90b671670a807f7876bfcface4c981ba72d1dafd
sivakiran-09 <polusiva.kiran2023@vitstudent.ac.in>
2026-01-08 18:36:54 +0530

    modified main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+students = []
+
+sid = input("Enter Student ID: ")
+name = input("Enter Name: ")
+dept = input("Enter Department: ")
+
+students.append([sid, name, dept])
+
+print("Student Registered Successfully")
+print("Registered Students:", students)
